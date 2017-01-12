71be110996aa9868c6ecb4c49ea2c8658834744c
Michael Seltzer <michael.m.seltzer@gmail.com>
2017-01-12 08:03:00 -0700

    messaging for errors and success

--- view/scripts/AppCtrl.js
+++ view/scripts/AppCtrl.js
@@ -1,5 +1,5 @@
 angular.module('hhb', ['ngMaterial'])
-.controller('AppCtrl', function($scope, $http) {
+.controller('AppCtrl', function($scope, $http, $mdToast) {
   $scope.isSettingVacationMode = false;
   $scope.failedSettingVacationMode = false;
   $scope.showAlertRules = false;
@@ -24,19 +24,32 @@ angular.module('hhb', ['ngMaterial'])
       $scope.status = res.data.status;
     },
     function(res){
+      $mdToast.show(
+        $mdToast.simple()
+          .textContent('Failed to retrieve device status')
+          .hideDelay(3000)
+      );
       $scope.updated = false;
     });
   }
 
   $scope.getAlertRules = function(){
+    $scope.isLoadingAlertRules = true;
     $scope.alertRules = [];
     $http.get('/api/alertRules').then(function(res){
+      $scope.isLoadingAlertRules = false;
       res.data.alerts.forEach(function(alert){
         if (!$scope.alertRules[alert.macAddress]){
           $scope.alertRules[alert.macAddress] = [];
         }
         $scope.alertRules[alert.macAddress].push(alert);
       })
+    }, function(){
+      $mdToast.show(
+        $mdToast.simple()
+          .textContent('Failed to retrieve alert rules')
+          .hideDelay(3000)
+      );
     });
 
   }
@@ -74,8 +87,18 @@ angular.module('hhb', ['ngMaterial'])
     }
     $http.post('/api/alertRules', {'alerts':rules}).then(function sucess(res){
       $scope.isSendingRules = false;
+      $mdToast.show(
+        $mdToast.simple()
+          .textContent('Saved!')
+          .hideDelay(3000)
+      );
     }, function failed(res){
       $scope.isSendingRules = false;
+      $mdToast.show(
+        $mdToast.simple()
+          .textContent('Failed to save alert rules')
+          .hideDelay(3000)
+      );
     });
   }
 
@@ -85,10 +108,20 @@ angular.module('hhb', ['ngMaterial'])
       $scope.getDevices();
       $scope.isSettingVacationMode = false;
       $scope.failedSettingVacationMode = false;
+      $mdToast.show(
+        $mdToast.simple()
+          .textContent('Saved!')
+          .hideDelay(3000)
+      );
     },
     function(res){
       $scope.failedSettingVacationMode = true;
       $scope.isSettingVacationMode = false;
+      $mdToast.show(
+        $mdToast.simple()
+          .textContent('Failed to save vacation mode settings')
+          .hideDelay(3000)
+      );
     });
   }
 
